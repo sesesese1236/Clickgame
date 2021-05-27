56dbd74c2cc14050e4cc0289dd3ce409973e4733
先生 <ateacher@iMac202003noiMac.local>
2021-05-27 16:13:58 +0900

    Database, Delete Button Reset Button

--- ClickGame/ViewController.swift
+++ ClickGame/ViewController.swift
@@ -6,7 +6,17 @@
 //
 
 import UIKit
+import RealmSwift
 
+class ClearTime: Object{
+    @objc dynamic var id: String = NSUUID().uuidString
+    @objc dynamic var time:String? = nil
+    @objc dynamic var scoreTime:Double = 0.0
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
 class ViewController: UIViewController {
 
     @IBOutlet weak var Lbl: UILabel!
@@ -37,6 +47,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let config = Realm.Configuration(schemaVersion: 4,
+                                         migrationBlock: { migration, oldSchemaVersion in
+                                            if oldSchemaVersion < 4 {
+                                                migration.enumerateObjects(ofType: ClearTime.className()) { oldObject, newObject in newObject!["id"] = NSUUID().uuidString
+                                                }
+                                            }
+                                         })
+        Realm.Configuration.defaultConfiguration = config
+        
         btn12=[btn1,btn2,btn3,
                btn4,btn5,btn6,
                btn7,btn8,btn9]
@@ -62,6 +81,80 @@ class ViewController: UIViewController {
     }
     @IBAction func Clear(_ sender: UIButton) {
         start()
+    }
+    @IBAction func Save(_ sender: UIButton) {
+        let cleartime:ClearTime = ClearTime()
+        
+        cleartime.time = String(scoreTime)
+        cleartime.scoreTime = scoreTime
+        
+        let realm = try! Realm()
+        
+        print("---Saving---")
+        
+        try! realm.write{
+            realm.add(cleartime)
+        }
+        
+        let cleartimelist:Results<ClearTime> =
+            realm.objects(ClearTime.self)
+        
+        print("---loading saved data---")
+        for cleartime in cleartimelist{
+            if cleartime.time != nil{
+                print(cleartime.id)
+                print(cleartime.time!)
+                print(cleartime.scoreTime)
+                print()
+            }else{
+                print("data not saved properly")
+            }
+        }
+    }
+    @IBAction func Search(_ sender: UIButton) {
+        let realm = try! Realm()
+        print("---loading saved data---")
+        
+        let fastTimeScoreList:Results<ClearTime> =
+            realm.objects(ClearTime.self).filter("scoreTime > 0.0 AND  scoreTime < 5")
+        for cleartime in fastTimeScoreList {
+            if cleartime.time != nil {
+                print(cleartime.id)
+                print(cleartime.time!)
+                print(cleartime.scoreTime)
+                print()
+            }
+            else{
+                print("data not saved properly")
+            }
+        }
+    }
+    @IBAction func Delete(_ sender: UIButton) {
+        let realm = try! Realm()
+        
+        print("---loading saved data---")
+        
+        let zeroTimeScoreList:Results<ClearTime> =
+            realm.objects(ClearTime.self).filter("scoreTime = 0.0")
+        
+        try! realm.write {
+            realm.delete(zeroTimeScoreList)
+        }
+        print("---show delete result---")
+        
+        let cleartimelist:Results<ClearTime> = realm.objects(ClearTime.self)
+        
+        for cleartime in cleartimelist {
+            if cleartime.time != nil{
+                print(cleartime.id)
+                print(cleartime.time!)
+                print(cleartime.scoreTime)
+                print()
+            }
+            else{
+                print("data not saved properly")
+            }
+        }
     }
     func start(){
         i=0
